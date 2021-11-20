a7451562af7c7a24a61e48284486a39b6d20bf6c
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2021-11-20 20:20:17 +0900

    Add files via upload

---
+++ src/algorithms/searching/linear-search.js
@@ -0,0 +1,33 @@
+function randomArrayMaker() {
+  let arr = [];
+  for(let i = 0; i < 10; i ++)
+    arr.push(Math.floor(Math.random() * 11)); 
+  return arr;
+}
+
+function printArray(arr) {
+  
+  for(let i = 0; i < arr.length; i++)
+    process.stdout.write(arr[i]+' ');
+
+}
+
+function linearSearch(arr) {
+  key = 5
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === key){
+      return `\nfirst key index is ${i}`;
+    }
+  }
+  return '\nno key value';
+  
+}
+
+let arr = randomArrayMaker();
+printArray(arr);
+console.log(linearSearch(arr));
+
+
+
+
+
