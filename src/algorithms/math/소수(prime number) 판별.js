c86c7ba963ed06410655716b5f06107ec13b352b
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2021-12-11 17:54:03 +0900

    Update 소수(prime number) 판별.js

--- src/algorithms/math/소수(prime number) 판별.js
+++ src/algorithms/math/소수(prime number) 판별.js
@@ -1,5 +1,4 @@
 function isPrimeNum(num) {
-
   for(let i = 2; i <= Math.floor(num**0.5); i++){
     if(num % i === 0){
       console.log('소수가 아닙니다.');
@@ -8,8 +7,7 @@ function isPrimeNum(num) {
       console.log('소수입니다.')
       return;
     }
-  }
-  
+  }  
 }
 
 isPrimeNum(100);
