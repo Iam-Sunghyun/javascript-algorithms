ba47829213b05091f0c61419482d0d119594cf58
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2021-12-20 21:57:02 +0900

    Add files via upload

---
+++ src/data-structures/tree/binary-search-tree.js
@@ -0,0 +1,56 @@
+import BinarySearchTreeNode from "./binary-search-tree-node.js";
+
+export default class BinarySearchTree{
+
+  constructor(){
+    this.root = new BinarySearchTreeNode(null);
+  }
+
+  insert(value){
+    return this.root.insert(value);
+  }
+
+  search(value){
+    return this.root.search(value);
+  }
+
+  delete(value){
+    return this.root.delete(value);
+  }
+
+  preOrder(){
+    this.root.preOrder();
+  }
+  inOrder(){
+    this.root.inOrder();
+  }
+  postOrder(){
+    this.root.postOrder();
+  }
+  levelOrder(){
+    this.root.levelOrder();
+  }
+}
+
+
+const BST = new BinarySearchTree();
+
+BST.insert(35);
+BST.insert(18);
+BST.insert(68);
+BST.insert(7);
+
+console.log(BST.search(7));
+console.log(BST.delete(18));
+
+BST.inOrder();
+
+
+
+
+
+
+
+
+
+
