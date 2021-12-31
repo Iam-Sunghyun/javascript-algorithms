dac024ba58f2a2e158d3e0660ed36662c9a1d28b
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2021-12-31 15:45:50 +0900

    메서드명 수정

--- src/data-structures/heap/maxheap.js
+++ src/data-structures/heap/maxheap.js
@@ -2,7 +2,7 @@
 class MaxHeap{
 
   constructor(){
-    this.maxheap = [null];
+    this.maxheap = [null];  // 0번은 사용하지 않음.
   }
 
   swap(a,b){
@@ -11,15 +11,15 @@ class MaxHeap{
     this.maxheap[b] = tmp;
   }
 
-  parentIndex(i){
+  parent(i){
     return Math.floor(i/2);
   }
 
-  leftChildIndex(i){
+  leftChild(i){
     return Math.floor(i*2);
   }
   
-  rightChildIndex(i){
+  rightChild(i){
     return Math.floor(i*2) + 1;
   }
 
@@ -32,30 +32,33 @@ class MaxHeap{
   insert(node){
    
     this.maxheap.push(node);         // 말단에 새 노드 삽입
-    let currentIndex = this.maxheap.length - 1;     // 새 노드 인덱스
+    let current = this.maxheap.length - 1;     // 새 노드 인덱스
 
-    while(currentIndex > 1){         // 부모 노드가 루트 노드 일 때까지 비교
-      if (this.maxheap[currentIndex] > this.maxheap[this.parentIndex(currentIndex)] && this.parentIndex(currentIndex) >= 1){
-        this.swap(this.parentIndex(currentIndex) , currentIndex);
-        currentIndex = this.parentIndex(currentIndex);
+    // 업 힙(up-heap)
+    while(current > 1){         // 부모 노드가 루트 노드 일 때까지 
+      if (this.maxheap[current] > this.maxheap[this.parent(current)] && this.parent(current) >= 1){
+        this.swap(this.parent(current) , current);                    // ↑ 이건 while문 조건이 currentIndex > 1이라 없어도 될 듯.
+        current = this.parent(current);
       }else break;
 
     }
   }
 
   delete(){
-    let currentIndex = this.maxheap.length - 1; 
-    if (this.maxheap.length <= 0) return;
-    this.swap(1,currentIndex);
+    let current = this.maxheap.length - 1; 
+    if (this.maxheap.length <= 1) return;
+
+    this.swap(1,current);
     let maxValue = this.maxheap.pop();
     console.log(`최대 값 = ${maxValue}`);
     this.maxHeapify(1);   
     return maxValue;
   }
-
-  maxHeapify(i){
-    let leftChild = this.rightChildIndex(i);
-    let rightChild = this.leftChildIndex(i);
+  
+  // 다운 힙(down-heap)
+  maxHeapify(i){  
+    let leftChild = this.rightChild(i);
+    let rightChild = this.leftChild(i);
     let maxIndex = i;
 
     if (leftChild <= this.maxheap.length - 1 && this.maxheap[leftChild] > this.maxheap[maxIndex] )
