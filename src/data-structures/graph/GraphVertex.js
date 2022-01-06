1a0883d0545596e7eddd8deaa2d3adbe767fd5b5
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-01-06 20:20:58 +0900

    생성자 추가

--- src/data-structures/graph/GraphVertex.js
+++ src/data-structures/graph/GraphVertex.js
@@ -1,6 +1,28 @@
 export default class GraphVertex {
   
-  constructor(value){
-  
+  /**
+   * @param {*} value
+   */
+  constructor(value) {
+    if (value === undefined) {
+      throw new Error('Graph vertex must have a value');
+    }
+
+    /**
+     * @param {GraphEdge} edgeA
+     * @param {GraphEdge} edgeB
+     */
+    const edgeComparator = (edgeA, edgeB) => {
+      if (edgeA.getKey() === edgeB.getKey()) {
+        return 0;
+      }
+
+      return edgeA.getKey() < edgeB.getKey() ? -1 : 1;
+    };
+
+    // Normally you would store string value like vertex name.
+    // But generally it may be any object as well
+    this.value = value;
+    this.edges = new LinkedList(edgeComparator);
   }
 }
