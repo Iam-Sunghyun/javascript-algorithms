1c7e8fc9cb247b75678398fe59ccffe7ca35a1ce
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-01-23 17:47:27 +0900

    Update Graph.js

--- src/data-structures/graph/Graph.js
+++ src/data-structures/graph/Graph.js
@@ -1,5 +1,10 @@
-class Graph{
-  constructor(){
-  
-  }  
+export default class Graph{
+   /**
+   * @param {boolean} isDirected
+   */
+  constructor(isDirected = false) {
+    this.vertices = {};
+    this.edges = {};
+    this.isDirected = isDirected;
+  }
 }
