dc41c3532254dde51947247e00db601c1e396266
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-03-10 22:51:47 +0900

    Create isPowerOfTwo.js

---
+++ src/algorithms/math/isPowerOfTwo.js
@@ -0,0 +1,19 @@
+// 2의 거듭제곱 판별
+export default function isPowerOfTwo(num){
+ 
+  if (num < 1) {
+    return false;
+  }
+  
+  let dividedNum = number;
+  while (dividedNum !== 1) {
+    
+    if (dividedNum % 2 !== 0) {
+      return false;
+    }
+    
+    dividedNum /= 2;
+  }
+  
+  return true;
+}
