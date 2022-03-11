e5b5da94485c064d02221572a61834f25e0af015
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-03-11 23:44:09 +0900

    Update euclideanAlgorithm.js

--- src/algorithms/math/euclideanAlgorithm.js
+++ src/algorithms/math/euclideanAlgorithm.js
@@ -1,3 +1,11 @@
+/**
+ * 최대 공약수(Greatest Common Multiple) 구하기
+ *
+ * @param {number} originalA
+ * @param {number} originalb
+ * return {number}
+ */
+
 export default function euclideanAlgorithm(originalA, originalB) {
   
   const a = Math.abs(originalA);
