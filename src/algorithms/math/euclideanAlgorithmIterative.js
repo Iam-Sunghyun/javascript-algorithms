33d8c9f9734fb17b58058a95eff6711b312e1fad
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-03-14 23:38:50 +0900

    Update euclideanAlgorithmIterative.js

--- src/algorithms/math/euclideanAlgorithmIterative.js
+++ src/algorithms/math/euclideanAlgorithmIterative.js
@@ -1,9 +1,16 @@
 /**
  * 최대 공약수(Greatest Common Divisor) 구하기 (반복)
  *
- *
- *
+ * @param {number} A
+ * @param {number} B
+ * @return {number}
  */
-export default function euclideanAlgorithmIterative(originalA, originalB) {
+export default function euclideanAlgorithmIterative(A, B) {
+  const a = Math.abs(A);
+  const b = Math.abs(B);
   
+  while(b !== 0){
+    [a , b] = [b , a % b];
+  }
+  return a;
 }
