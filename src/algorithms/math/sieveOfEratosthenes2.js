39aa453031a7cc1016fb51096a60a45667998748
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-03-19 22:15:28 +0900

    Update sieveOfEratosthenes2.js

--- src/algorithms/math/sieveOfEratosthenes2.js
+++ src/algorithms/math/sieveOfEratosthenes2.js
@@ -1,1 +1,10 @@
-
+/**
+ *
+ * @param {number} Number
+ * @return {number[]}
+ */
+export default function sieveOfEratosthenes(Number) {
+  const prime = new Array(Number + 1).fill(true);
+  
+  
+}
