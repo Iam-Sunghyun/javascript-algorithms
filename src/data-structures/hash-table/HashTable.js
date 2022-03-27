4a6db9b0f4086f8d886b58a090ddddd51dfd50f5
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-03-27 22:19:00 +0900

    Update HashTable.js

--- src/data-structures/hash-table/HashTable.js
+++ src/data-structures/hash-table/HashTable.js
@@ -1,8 +1,13 @@
 import LinkedList from '../linked-list/linked-list.js';
 
+
+const defaultHashTableSize = 32;
+
 export default class HashTable {
  
-  construct(){
+  construct(hashTableSize = defaultHashTableSize){
+   // 매개변수 크기만큼의 배열을 만들고, 각 요소에 빈 연결리스트(헤드)를 채움. 
+   this.buckets = Array(hashTableSize).fill(null).map(() => new LinkedList());
   }
   
 }
