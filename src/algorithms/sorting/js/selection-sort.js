06921d5dcb33dee3c4800f077bc5512071078040
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-04-02 00:20:54 +0900

    Update selection-sort.js

--- src/algorithms/sorting/js/selection-sort.js
+++ src/algorithms/sorting/js/selection-sort.js
@@ -1,47 +1,31 @@
+export default function selectionSort(originalArray){
 
-function swap(arr,a,b){
+  let minIndex;
+  // 원본 보존
+  const arr = [...originalArray];
 
-  /* let tmp = arr[a]
-     arr[a] = arr[b]
-     arr[b] = tmp  */
-  [arr[a],arr[b]] = [arr[b],arr[a]]
-
-}
-
-function selectionSort(arr,n){
-
-  let i ,j, minIndex;
-
-  for(i = 0; i < n-1; i++){
-
+  for(let i = 0; i < arr.length - 1; i++){
     minIndex = i
-    for(j = i + 1; j < n; j++){
+
+    for(let j = i + 1; j < arr.length; j++){
       if(arr[minIndex] > arr[j])
         minIndex = j;
     }
-    swap(arr,minIndex,i);
+
+    [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
   }
+  
+  return arr;
 }
 
-function printArray( arr,  size)
-{
-    let i;
-    for (i = 0; i < size; i++)
-        document.write(arr[i] + " ");
-    document.write(" <br>");
-}
-
-
-let arr = [];
-for (i = 0; i < 15; i++)
+const arr = [];
+for (let i = 0; i < 15; i++){
   arr.push(Math.floor(Math.random() * 100));
+}
 
-let n = arr.length;
-document.write("UnSorted array: \n");
-printArray(arr, n);
-
-selectionSort(arr, n);
-document.write("Sorted array: \n");
-printArray(arr, n);
-
-     
+console.log("UnSorted array: ");
+console.log(arr);
+ 
+const sorted = selectionSort(arr);
+console.log("Sorted array: ");
+console.log(sorted);
