042618ef5610928b1c1d3233a2e2b4afd880f16e
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-04-02 22:39:49 +0900

    Update shell-sort.js

--- src/algorithms/sorting/js/shell-sort.js
+++ src/algorithms/sorting/js/shell-sort.js
@@ -1,22 +1,31 @@
+export default function shellSort(originalArray){
+
+  // 원본 변경 방지
+  const arr = [...originalArray];
+
+  let gap = Math.floor(arr.length / 2);
+
+  while (gap > 0){
  
-function shellSort(arr,n){
+    // gap이 짝수인 경우 홀수로 만들어 줌.
+    // gap = gap % 2 ? gap : gap + 1;
 
-  let i, gap, temp;
-  let j; 
-  for (gap = Math.floor(n/2); gap > 0; gap = Math.floor(gap/2)){
+    for(let i = gap; i < arr.length; i++ ){
+      let j;
+      let temp = arr[i];
 
-    // gap이 짝수인 경우 +1
-    if (gap % 2 === 0) gap++;
-    for(i = gap; i < n; i++ ){
-
-      temp = arr[i];
-      for(j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+      for(j = i; j >= gap && arr[j - gap] > temp; j -= gap){
         arr[j] = arr[j - gap];
+      }
 
       arr[j] = temp;
     }
+
+    gap = Math.floor(gap / 2)
   }
+  return arr;
 }
+
 
 function printArray(arr)
 {
@@ -26,22 +35,15 @@ function printArray(arr)
         console.log("\n");
 }
 
-let arr = [];
-for (i = 0; i < 10; i++)
+// 테스트
+const arr = [];
+for (let i = 0; i < 15; i++){
   arr.push(Math.floor(Math.random() * 100));
-
-let n = arr.length;
+}
 
 console.log("UnSorted array: ");
-printArray(arr, n);
-
-let startTime = new Date().getTime();
-shellSort(arr, n);
-let endTime = new Date().getTime();
-
+console.log(arr);
+ 
+const sorted = shellSort(arr);
 console.log("Sorted array: ");
-printArray(arr, n);
-// 수행 시간 측정 (s) 1000개 이상은 되어야 측정되기 시작함
-console.log((endTime-startTime) / 1000);
-
-
+console.log(sorted);
