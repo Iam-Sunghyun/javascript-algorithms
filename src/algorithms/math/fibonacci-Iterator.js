175ed86f34d5380235001e97e52d8e30c546bede
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-04-05 19:14:32 +0900

    Update fibonacci-Iterator.js

--- src/algorithms/math/fibonacci-Iterator.js
+++ src/algorithms/math/fibonacci-Iterator.js
@@ -7,12 +7,14 @@ const fibonacci = function(maxIndex){
   let count = 0;
   let [pre, cur] = [0, 1];
 
+  // 사용자 정의 이터러블 객체 반환
   return {
     [Symbol.iterator](){     
       return {
         next(){
           [pre, cur] = [cur, pre + cur];
           count += 1;
+          // 이터레이터 result 객체 반환
           return { value: cur, done: count >= maxIndex }; 
         }
       };
