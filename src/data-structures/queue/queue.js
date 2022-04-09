cab3a9bb04238be28bce3bcb20de450c6ea1594c
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-04-09 19:44:39 +0900

    Update queue.js

--- src/data-structures/queue/queue.js
+++ src/data-structures/queue/queue.js
@@ -1,38 +1,47 @@
 class Queue{
-
-  constructor(){
-    this.queue = [];
+  #queue // private 클래스 멤버
+  constructor(array = []){
+    if(!Array.isArray(array)){
+      throw new TypeError(`${array}는 배열이 아닙니다.`);
+    }
+    this.#queue = array;
   }
 
   enqueue(data){
-    this.queue.push(data);
+    this.#queue.push(data);
   }
 
   dequeue(){
-    return this.queue.shift();
+    return this.#queue.shift();
   }
 
   isEmpty(){
-    this.queue.length ? true : false;
+    this.#queue.length ? true : false;
   }
 
   peek(){
-    return this.queue[0];
+    return this.#queue[0];
+  }
+
+  entries(){
+    return [...this.#queue];
   }
 
   size(){
-    return this.queue.length;
+    return this.#queue.length;
   }
  
 }
 
-let q = new Queue();
+const q = new Queue();
 q.enqueue(1);
 q.enqueue(2);
 q.enqueue(3);
 q.enqueue(4);
 
 console.log(q.dequeue());
+console.log(q.dequeue());
+console.log(q.entries())
 console.log(q.dequeue());
 console.log(q)
 
