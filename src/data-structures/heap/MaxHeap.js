f5723d32be95c93650bee2a302e4fb53c46a0e8a
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-04-10 22:37:25 +0900

    Update MaxHeap.js

--- src/data-structures/heap/MaxHeap.js
+++ src/data-structures/heap/MaxHeap.js
@@ -6,20 +6,20 @@ class MaxHeap{
   }
 
   swap(a,b){
-    let tmp = this.maxheap[a];
+    const tmp = this.maxheap[a];
     this.maxheap[a] = this.maxheap[b];
     this.maxheap[b] = tmp;
   }
 
-  parent(i){
+  parentIndex(i){
     return Math.floor(i/2);
   }
 
-  leftChild(i){
+  leftChildIndex(i){
     return Math.floor(i*2);
   }
   
-  rightChild(i){
+  rightChildIndex(i){
     return Math.floor(i*2) + 1;
   }
 
@@ -36,29 +36,31 @@ class MaxHeap{
 
     // 업 힙(up-heap)
     while(current > 1){         // 부모 노드가 루트 노드 일 때까지 
-      if (this.maxheap[current] > this.maxheap[this.parent(current)] && this.parent(current) >= 1){
-        this.swap(this.parent(current) , current);                    // ↑ 이건 while문 조건이 currentIndex > 1이라 없어도 될 듯.
-        current = this.parent(current);
+      if (this.maxheap[current] > this.maxheap[this.parenIndext(current)] && this.parentIndex(current) >= 1){
+        this.swap(this.parentIndex(current) , current);                    // ↑ 이건 while문 조건이 currentIndex > 1이라 없어도 될 듯.
+        current = this.parentIndex(current);
       }else break;
 
     }
   }
 
   delete(){
-    let current = this.maxheap.length - 1; 
+    const current = this.maxheap.length - 1; 
     if (this.maxheap.length <= 1) return;
 
     this.swap(1,current);
-    let maxValue = this.maxheap.pop();
+    
+    const maxValue = this.maxheap.pop();
     console.log(`최대 값 = ${maxValue}`);
+    
     this.maxHeapify(1);   
     return maxValue;
   }
   
   // 다운 힙(down-heap)
   maxHeapify(i){  
-    let leftChild = this.rightChild(i);
-    let rightChild = this.leftChild(i);
+    let leftChild = this.rightChildIndex(i);
+    let rightChild = this.leftChildIndex(i);
     let maxIndex = i;
 
     if (leftChild <= this.maxheap.length - 1 && this.maxheap[leftChild] > this.maxheap[maxIndex] )
