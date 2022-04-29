65eca3051a2119ef485bdee33035cdb19345317a
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-04-30 00:34:50 +0900

    Update 신규 아이디 추천.js

--- src/algorithms/others/코딩테스트 연습/2021 KAKAO BLIND RECRUITMENT/신규 아이디 추천.js
+++ src/algorithms/others/코딩테스트 연습/2021 KAKAO BLIND RECRUITMENT/신규 아이디 추천.js
@@ -1,36 +1,28 @@
+/**
+ * 2021 KAKAO BLIND RECRUITMENT (https://programmers.co.kr/learn/courses/30/lessons/72410#)
+ * @param {string} new_id 
+ * @returns {string} answer
+ */
 function solution(new_id) {
   let answer = '';
 
-  // 정규 표현식으로 더 간결하게 할 수 있을 듯
-  answer = new_id.toLowerCase();
-  answer = answer.replace(/[^A-Za-z0-9\-_.]/g, '')
+  // 정규 표현식 쓸줄 알면 아주 쉬운 문제
+  answer = new_id.toLowerCase()
+                 .replace(/[^A-Za-z0-9\-_.]/g, '')
                  .replace(/[^A-Za-z0-9\-_]+/g, '.')
-                 //.replace(/^[^A-Za-z0-9\-_]+$/g, '')
-                 //.trim().slice(0,16);
-
-                 console.log(answer)
+                 .replace(/^\.|\.$/g, '')
+                 .replace(/^$/g, 'a')
+                 .slice(0,15)
+                 .replace(/\.$/g, '');
   
-  if(answer[0] === '.'){
-    answer = answer.slice(1,);
-  }
-  answer = answer.trim().slice(0,15);
-
-  if(answer[answer.length - 1] === '.'){
-    answer = answer.slice(0,answer.length - 1);
-  }
-
-  if(answer.length === 0){
-    answer += 'a';
-  }
-  
-  if(answer.length <= 2){
-    const lastSpell = answer[answer.length - 1];
-    
-    while(answer.length < 3){
-      answer += lastSpell
-    }
-  }
-  
+  if(answer.length === 1) answer += answer[0].repeat(2);
+  if(answer.length === 2) answer += answer[1].repeat(1);
 
   return answer;
 }
+
+console.log(solution("...!@BaT#*..y.abcdefghijklm"));
+console.log(solution("z-+.^."));
+console.log(solution("abcdefghijklmn.p"));
+console.log(solution("123_.def"));
+console.log(solution("=.="));
