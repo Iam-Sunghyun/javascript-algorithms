3073d2a834c301328b05dd9ca9be818eae3c9619
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-04-29 19:51:43 +0900

    주석 수정

--- src/algorithms/others/코딩테스트 연습/정렬/k번째 수.js
+++ src/algorithms/others/코딩테스트 연습/정렬/k번째 수.js
@@ -1,7 +1,7 @@
 /**
  * 정렬-k번째 수 (https://programmers.co.kr/learn/courses/30/lessons/42748)
  * @param {number[]} array 배열 array
- * @param {number[[]]} commands [i, j, k]를 원소로 가진 2차원 배열 commands 
+ * @param {number[][]} commands [i, j, k]를 원소로 가진 2차원 배열 commands 
  * @returns {number[]} answer 연산을 적용했을 때 나온 결과를 배열
  */
 function solution(array, commands) {
