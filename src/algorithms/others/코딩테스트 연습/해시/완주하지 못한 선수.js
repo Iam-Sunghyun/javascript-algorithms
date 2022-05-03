e440cc580e3fe4fc9648c9398e65b11ee1618b02
Iam-Sunghyun <46747113+Iam-Sunghyun@users.noreply.github.com>
2022-05-03 15:29:38 +0900

    Update 완주하지 못한 선수.js

--- src/algorithms/others/코딩테스트 연습/해시/완주하지 못한 선수.js
+++ src/algorithms/others/코딩테스트 연습/해시/완주하지 못한 선수.js
@@ -1,1 +1,23 @@
+/**
+ * (https://programmers.co.kr/learn/courses/30/lessons/42576)
+ * @param {string[]} participant 
+ * @param {string[]} completion 
+ * @returns {string} answer
+ */
+function solution(participant, completion) {
 
+  participant.sort();
+  completion.sort();
+  console.log(participant, completion)
+  for(let i = 0; i < completion.length; i++){
+    if(participant[i] !== completion[i]){
+      return participant[i];
+    }
+  }
+  
+  return participant[i - 1];
+}
+
+console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"]));
+console.log(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]));
+console.log(solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"]));
