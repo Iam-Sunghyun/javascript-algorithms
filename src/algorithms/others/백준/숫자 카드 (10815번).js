88ec0eea8bed58d6a9f28548ffe8fe6eae69ad17
전성현 <sunghyun1148@gmail.com>
2022-05-10 00:58:41 +0900

    주석 추가

--- src/algorithms/others/백준/숫자 카드 (10815번).js
+++ src/algorithms/others/백준/숫자 카드 (10815번).js
@@ -1,6 +1,11 @@
 // 백준 10815번 (https://www.acmicpc.net/problem/10815)
-// 입력 받는 코드 ↓
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+// 입력 받는 코드 ↓ (백준에서 사용 가능한 간략 버전(fs))
+//const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
 const [N, M] = [+input[0], +input[2]];
 const [hasCards, givenCards] = [new Set(input[1].split(' ').map(Number)), input[3].split(' ').map(Number)];
 
