5283b4790d4d711827d2d960c3f5f256efb0575a
전성현 <sunghyun1148@gmail.com>
2022-05-24 23:15:24 +0900

    백준 디렉토리 수정 및 문제 풀이

---
+++ src/algorithms/others/백준/집합과 맵/14425번 문자열 집합.js
@@ -0,0 +1,23 @@
+// 14425번 문자열 집합 https://www.acmicpc.net/problem/14425
+// 입력 받기 ↓
+const filePath = process.platform === 'linux' ? 'dev/stdin' : './input.txt'
+const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
+
+
+function hasString(input){
+
+  const n = input[0].split(' ')[0];
+  // 문자열 카드 나누기
+  const a = new Set(input.splice(1, +n));
+  const b = input.splice(1);
+  let answer = 0;
+
+  // 교집합 검사
+  for(const string of b){
+    if(a.has(string)) answer += 1;
+  }
+  return answer;
+}
+
+
+console.log(hasString(input));
