8a6be850ab17cf263a47906e4ae18bf72bd7a330
전성현 <sunghyun1148@gmail.com>
2022-05-27 16:38:47 +0900

    3번째 효율성 수정 (정답코드)

--- src/algorithms/others/프로그래머스/2017 팁스타운/짝지어 제거하기.js
+++ src/algorithms/others/프로그래머스/2017 팁스타운/짝지어 제거하기.js
@@ -1,15 +1,15 @@
 /**
- * 2번째 수정코드 - 처음 코드보다 약 2~11배 속도 줄였지만 여전히 효율성 테스트 실패
+ * 3번째 수정코드(정답코드) - 2번째 코드보다 대략 20~100배 이상, 첫 코드 보다 몇천배 실행 속도 줄어듦
  * 2017 팁스타운 레벨2 https://programmers.co.kr/learn/courses/30/lessons/12973
  * @param {string[]} s 
  * @returns {number}
  */
  function solution(s){
-  const str = [...s];
+  const str = [...s].reverse();
   const stack = [];
   while(true){
-    // 효율성 실패의 원인 shift()
-    stack.push(str.shift())
+    // shift()를 pop()으로 변경 시켜주니 실행속도 대폭 줄어듦
+    stack.push(str.pop())
     if(stack[stack.length -1] === stack[stack.length - 2]){
       stack.pop();
       stack.pop();
@@ -18,7 +18,6 @@
     if(str.length === 0 && stack.length === 0) return 1;
   }
 }
-
 console.log(solution('baabaa')); // 1
 console.log(solution('cdcd')); // 0
 console.log(solution('cccdcd')); // 0
