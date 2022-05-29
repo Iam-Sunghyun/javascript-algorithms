3e469c8682e1b2160619049fc77bf8ef463e1904
전성현 <sunghyun1148@gmail.com>
2022-05-30 00:08:56 +0900

    주석 약간 추가

--- src/algorithms/others/frequencyCounter.js
+++ src/algorithms/others/frequencyCounter.js
@@ -3,8 +3,8 @@
  * 공식 명칭은 아니지만 객체의 값과 빈도를 수집하는데 널리 사용하는 패턴으로 알아 둘 필요가 있다
  * 사용 예) 입력 값이 서로 비슷한 값으로 구성되어 있는지, 아나그램인지, 특정 값이 다른 값에 같은 빈도로 포함되어 있는지, 각종 객체에 특정 값의 빈도를 체크 등등
  * 주로 배열, 문자열 비교에 필요한 2중 for문과 같은 O(n^2) 연산을 대체하기 위해 사용 되는 패턴으로 O(n)정도 복잡도를 갖는다
+ * 요소를 하나하나 비교하는 것이 아닌, 객체를 생성해 값의 빈도수를 세고, 그 빈도수를 비교하며 체크한다
  */
-
 // 배열 arr1의 요소의 제곱이 다른 배열 arr2의 요소로 있는 지 체크하는 함수 
 // 순진한(naive) 방법 O(n^2)
 function same(arr1, arr2){
@@ -58,7 +58,7 @@ function checkPerformance(f, n){
   console.log((t2 - t1) / 1000);
 }
 
-checkPerformance(same, 100000);
-checkPerformance(improvedSame, 100000); // 10만개 기준 약 9배 빠름
+// checkPerformance(same, 100000);
+// checkPerformance(improvedSame, 100000); // 10만개 기준 약 9배 빠름
 
-console.log(improvedSame([1,2,3,11], [1,4,9,1])); // false;
+console.log(improvedSame([1,2,3,11,5], [2,1,4,9,1])); // false;
