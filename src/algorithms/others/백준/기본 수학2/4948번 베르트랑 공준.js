ff5825f7a3f3888523b34d4e225b7e7972263da1
전성현 <sunghyun1148@gmail.com>
2022-06-03 23:47:40 +0900

    백준 기본 수학2 소수 문제풀이 및 주석 수정

--- src/algorithms/others/백준/기본 수학2/4948번 베르트랑 공준.js
+++ src/algorithms/others/백준/기본 수학2/4948번 베르트랑 공준.js
@@ -1,18 +1,17 @@
-// 백준 1929번 기본 수학2 https://www.acmicpc.net/problem/1929
-// 2581번 함수를 그대로 재사용함. 
+// 백준 4948번 기본 수학2 https://www.acmicpc.net/problem/4948
+// 1978번 함수를 그대로 재사용함. 
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(/\s/);
+input.pop();
 
-const m = +input[0];
-const n = +input[1];
-
-function getPrimeNumberFromMtoN(M, N){
+function getPrimeNumberFromMtoN(num){
     // 에라토스테네스의체 연산을 위한 배열 준비
+    const N = num * 2;
+    const M = num + 1;
     const primeArray = new Array(N + 1).fill(true);
     primeArray[0] = false;
     primeArray[1] = false;
     
     const answerArray = [];
-
     // n이하의 소수를 에라토스테네의체를 이용해 구하고, 배열에 저장한다
     for(let i = 2; i <= Math.floor(N**0.5); i++){
         for(let j = i*i; j <= N; j += i){
@@ -23,7 +22,9 @@ function getPrimeNumberFromMtoN(M, N){
     // primeArray 배열에서 소수이면서 m이상인 값들만 answer 배열에 저장한다
     primeArray.forEach((num, i) => { if(num === true && i >= M) answerArray.push(i) });
     
-    console.log(answerArray.join('\n'));
+    console.log(answerArray.length);
 }
 
-getPrimeNumberFromMtoN(m, n);
+for(let i = 0; i < input.length; i++){
+    getPrimeNumberFromMtoN(+input[i]);
+}
