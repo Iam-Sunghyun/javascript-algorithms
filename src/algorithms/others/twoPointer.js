34341a4b8d26d78522597dfe6785f6d89e8aa35f
전성현 <sunghyun1148@gmail.com>
2022-06-10 20:34:09 +0900

    파일 이름 변경multiplePointer.js -> twoPointer.js

--- src/algorithms/others/twoPointer.js
+++ src/algorithms/others/twoPointer.js
@@ -1,29 +1,64 @@
 /**
- * - 투 포인터(Two Pointer) 알고리즘 예제
- * 오름차순 정렬된 2개의 배열을 입력받아 하나의 정렬된 배열로 합치는 함수
- * 2개의 배열을 concat한 뒤 sort해주면 훨씬 간단하지만 O(nlogn)의 시간복잡도를 갖는다
- * 다음과 같이 투 포인터 패턴을 사용하면 O(n+m) 만큼의 효율을 보인다 (n, m은 각각 입력받은 두 배열의 길이)
- * @param {number[]} arr1 
- * @param {number[]} arr2 
+ * - 다중 포인터(multiple pointer, 투 포인터)
+ * 선형 자료구조(배열, 연결리스트, 문자열)에서 인덱스나 값을 가리키는 2개 이상의 포인터를
+ * 특정 조건에 따라 이동시키면서 원하는 값이나 구간을 찾는 알고리즘 패턴
+ * 같은 지점으로 이동하거나(끝에서 시작으로 or 시작에서 끝으로), 양끝에서 중간지점으로 이동하거나,
+ * 한 포인터는 한방향으로 이동하고 다른 포인터는 양방향으로 이동해가는 방식 등이 있다
+ * 공간복잡도도 효율적이다(따로 공간 사용 no)
+ * 
+ * - 오름차순 정렬된 정수 배열을 매개변수로 받아 두 요소의 합이 target값과 일치하는 첫번째 쌍을 찾아내는 함수
+ * 1. 2중 for문을 사용한 단순한 방법 O(n^2)
+ * 간단하지만 효율이 좋지 않은 방법
+ * @param {number[]} arr 
  * @returns {number[]}
  */
-function solution(arr1, arr2) {
-  const answer = [];
-  // two pointer
-  let left = 0;
-  let right = 0;
-  
-  // 2개의 배열 중 하나라도 포인터가 배열의 유효한 범위 내에 있으면 계속된다
-  while(arr1[left] || arr2[right]){
-    if(arr1[left] < arr2[right]){
-      answer.push(arr1[left]);
-      left++;
-    }else{
-      answer.push(arr2[right]);
-      right++;
+function sumZero(arr, target){
+  // 2중 for문으로 모든 요소를 하나하나 비교한다
+  for(let i = 0; i < arr.length; i++){
+    for(let j = i + 1; j < arr.length; j++){
+      if(arr[i] + arr[j] === target){
+        return [arr[i], arr[j]];
+      }
     }
   }
-  return answer;
+  return -1;
 }
-console.log(solution([1,3,5], [2,3,6,7,9])); // [1,2,3,3,5,6,7,9]
-console.log(solution([1,2,3], [3,4,5,7])); // [1,2,3,3,4,5,7]
+console.log(sumZero([-4,-3,-2,-1,0,1,2,5], 0)); // [-2, 2]
+
+// 2. 투 포인터를 이용한 개선된 방법 O(n)
+// 두 포인터가 양 끝 지점에서 시작하여 중앙쪽으로 이동하며 값 쌍을 찾아낸다
+// 1번의 반복으로 원하는 값 쌍을 찾아낸다
+function pointerSumZero(arr, target){
+  let left = 0;
+  let right = arr.length - 1;
+  while(left < right){
+    const sum = arr[left] + arr[right];
+    // 두 요소 합이 target과 일치시 return
+    if(sum === target) return [arr[left], arr[right]];
+
+    // 두 요소 합이 target보다 크면 right을 감소
+    else if(sum > target) right--;
+
+    // 두 요소 합이 target보다 작면 left를 증가
+    else left++;
+    
+  }
+  return -1;
+}
+
+console.log(sumZero([-4,-3,-2,-1,0,1,2,5], 0)); // [-2, 2]
+
+// 시간 체크용 함수
+function checkPerformance(f, n){
+  const arr = [];
+  for(let i = -(n/2); i < n/2; i++){
+    arr.push(i);
+  }
+  const t1 = performance.now();
+  f(arr);
+  const t2 = performance.now();
+  console.log((t2 - t1) / 1000);
+}
+
+checkPerformance(sumZero, 10000);
+checkPerformance(pointerSumZero, 10000); // 투 포인터 패턴이 훨씬 빠른 실행속도를 보인다
