614c8e0197d02737ddfcf04bdf5bca66c275c31c
전성현 <sunghyun1148@gmail.com>
2022-06-10 22:17:55 +0900

    선형 탐색 코드 수정

--- src/algorithms/searching/linear-search.js
+++ src/algorithms/searching/linear-search.js
@@ -1,17 +1,11 @@
-function randomArrayMaker() {
-  let arr = [];
-  for(let i = 0; i < 10; i ++)
-    arr.push(Math.floor(Math.random() * 11)); 
-  return arr;
-}
+/**
+ * 선형 탐색(linear search) O(n)
+ * @param {number[]} arr 
+ * @param {number} key 
+ * @returns {number}
+ */
+function linearSearch(arr, key) {
 
-function printArray(arr) {
-  for(let i = 0; i < arr.length; i++)
-    process.stdout.write(arr[i]+' ');
-}
-
-function linearSearch(arr,key) {
-  key = 5
   for(let i = 0; i < arr.length; i++){
     if(arr[i] === key){
       return `\nfirst key index is ${i}`;
@@ -20,6 +14,21 @@ function linearSearch(arr,key) {
   return '\nno key value';
 }
 
-let arr = randomArrayMaker();
+// 난수 배열 생성 함수
+function randomArrayMaker() {
+  let arr = [];
+  for(let i = 0; i < 10; i ++)
+    arr.push(Math.floor(Math.random() * 11)); 
+  return arr;
+}
+
+// 배열 출력을 위한 함수
+function printArray(arr) {
+  for(let i = 0; i < arr.length; i++)
+    process.stdout.write(arr[i]+' ');
+}
+
+
+const arr = randomArrayMaker();
 printArray(arr);
-console.log(linearSearch(arr,5));
+console.log(linearSearch(arr, 1));
