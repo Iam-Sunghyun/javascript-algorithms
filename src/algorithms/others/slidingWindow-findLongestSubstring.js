6bbd198d1183493e392bc602e7bb11839bae2cc1
전성현 <sunghyun1148@gmail.com>
2022-06-11 23:48:58 +0900

    슬라이딩 윈도우 예제 추가

---
+++ src/algorithms/others/slidingWindow-findLongestSubstring.js
@@ -0,0 +1,27 @@
+/**
+ * 슬라이딩 윈도우(sliding window) 예제
+ * - 인수로 전달받은 문자열에서 고유값들로 이루어진 가장 긴 문자열을 반환하는 함수
+ * @param {strung} str 
+ * @returns {number}
+ */
+function findLongestSubstring(str){
+  let longest = 0;
+  let seen = {};
+  let start = 0;
+ 
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+    if (seen[char]) {
+      start = Math.max(start, seen[char]);
+    }
+  
+    longest = Math.max(longest, i - start + 1);
+    // 인덱스 저장
+    seen[char] = i + 1;
+  }
+  return longest;
+}
+
+//console.log(findLongestSubstring('bbbbbb')); // 1
+console.log(findLongestSubstring('rithmschool')); // 7
+
