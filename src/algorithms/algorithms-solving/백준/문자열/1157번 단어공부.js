118ea82280373bb163b6255d286acd64adb43de8
전성현 <sunghyun1148@gmail.com>
2022-06-15 23:06:52 +0900

    자동 포맷팅 적용

--- src/algorithms/algorithms-solving/백준/문자열/1157번 단어공부.js
+++ src/algorithms/algorithms-solving/백준/문자열/1157번 단어공부.js
@@ -2,17 +2,17 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 const string = [...input[0].toUpperCase()];
 
-function solution(str){
-    if(str.length === 1) return str[0];
-    const obj = {};
-    
-    str.forEach(str => {
-        obj[str] = ++obj[str] || 1;
-    });
-    
-    const answer = Object.entries(obj).sort((a, b) => b[1] - a[1]);
-    
-    return answer[0][1] === answer[1][1] ? '?' : answer[0][0];
+function solution(str) {
+  if (str.length === 1) return str[0];
+  const obj = {};
+
+  str.forEach((str) => {
+    obj[str] = ++obj[str] || 1;
+  });
+
+  const answer = Object.entries(obj).sort((a, b) => b[1] - a[1]);
+
+  return answer[0][1] === answer[1][1] ? '?' : answer[0][0];
 }
 
 console.log(solution(string));
