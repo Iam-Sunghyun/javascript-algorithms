f8f6a318a26ebd8a813440651d12833f287a431f
전성현 <sunghyun1148@gmail.com>
2022-06-22 23:19:23 +0900

    코드 수정 (splice()->slice() 및 경로 수정)

--- src/algorithms/algorithms-solving/백준/집합과 맵/14425번 문자열 집합.js
+++ src/algorithms/algorithms-solving/백준/집합과 맵/14425번 문자열 집합.js
@@ -1,20 +1,19 @@
 // 14425번 문자열 집합 https://www.acmicpc.net/problem/14425
 // 입력 받기 ↓
-const filePath = process.platform === 'linux' ? 'dev/stdin' : './input.txt'
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = require('fs').readFileSync(filePath).toString().trim().split('\n');
 
-
-function hasString(input){
+function hasString(input) {
 
   const n = input[0].split(' ')[0];
   // 문자열 카드 나누기
-  const a = new Set(input.splice(1, +n));
-  const b = input.splice(1);
+  const a = new Set(input.slice(1, +n));
+  const b = input.slice(n + 1);
   let answer = 0;
 
   // 교집합 검사
-  for(const string of b){
-    if(a.has(string)) answer += 1;
+  for (const string of b) {
+    if (a.has(string)) answer += 1;
   }
   return answer;
 }
