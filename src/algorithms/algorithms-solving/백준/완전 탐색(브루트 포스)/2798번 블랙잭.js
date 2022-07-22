439f475220c9ff2040d3fda327522be48514c848
전성현 <sunghyun1148@gmail.com>
2022-07-22 23:39:28 +0900

    백준 브루트포스 문제풀이

---
+++ src/algorithms/algorithms-solving/백준/완전 탐색(브루트 포스)/2798번 블랙잭.js
@@ -0,0 +1,21 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(/\n/);
+const list = input.slice(1)[0].split(' ').map(Number);
+const target = +input[0].split(' ')[1];
+
+function solution(n, target) {
+  const answer = [];
+  for (let i = 0; i < n.length; i++) {
+    for (let j = i + 1; j < n.length; j++) {
+      for (let k = j + 1; k < n.length; k++) {
+        if (n[i] + n[j] + n[k] <= target) {
+          answer.push([n[i] + n[j] + n[k], n[i], n[j], n[k]]);
+        }
+      }
+    }
+  }
+  answer.sort((a, b) => b[0] - a[0]);
+  return +answer[0][0];
+}
+
+
+console.log(solution(list, target));
