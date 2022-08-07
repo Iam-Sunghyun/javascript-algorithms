602f50d91858ff4e0f46feac0edc9c85c4b04a86
전성현 <sunghyun1148@gmail.com>
2022-08-07 23:43:38 +0900

    문제 번호 추가

--- src/algorithms/algorithms-solving/백준/완전 탐색(브루트 포스)/7568번 덩치.js
+++ src/algorithms/algorithms-solving/백준/완전 탐색(브루트 포스)/7568번 덩치.js
@@ -1,16 +1,16 @@
-// https://www.acmicpc.net/problem/7568
+// 백준 7568번 완전 탐색 https://www.acmicpc.net/problem/7568
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(/\n/);
 const list = input.slice(1);
 
 function solution(n) {
   // 등수 기록용 배열
-  const answer = new Array(n.length).fill(1); 
+  const answer = new Array(n.length).fill(1);
   // 입력 값 쓰기 쉽게 가공하기
   const list = n.map(spec => spec.split(' ').map(Number)); // 
 
   for (let i = 0; i < list.length; i++) {
     for (let j = 0; j < list.length; j++) {
-        if(i === j) continue;
+      if (i === j) continue;
       if (list[i][0] < list[j][0] && list[i][1] < list[j][1]) { // 키, 체중 모두 큰 사람이 있으면 등수 +1
         answer[i] += 1;
       }
