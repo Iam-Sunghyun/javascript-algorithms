830009a99f6a3b59f8c267ebd8e3bc8758b6b3ac
전성현 <sunghyun1148@gmail.com>
2022-08-28 20:22:51 +0900

    포맷팅, 틀린 코드 수정

--- src/algorithms/sorting/js/quick-sort.js
+++ src/algorithms/sorting/js/quick-sort.js
@@ -1,37 +1,41 @@
-function swap(arr,a,b) {
-    const temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
+function swap(arr, a, b) {
+  const temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
 }
 
-function quickSort(arr,start,end) {
-  if(end <= start)return;
-    let pivot = start;
-    let left = start + 1;
-    let right = end;
+function quickSort(arr, start, end) {
+  if (end <= start) return;
+  const pivot = start;
+  let left = start + 1;
+  let right = end;
 
-    while(right >= left){
-      
-      while(left <= end && arr[left] <= arr[pivot]){
-        left++;
-      }
-      while(right >= start && arr[right] >= arr[pivot]){
-        right--;
-      }
-      if(left > right){
-        swap(arr,pivot,right);
-      }else{
-        swap(arr,left,right);
-      }
+  while (right >= left) {
+
+    while (left <= end && arr[left] <= arr[pivot]) {
+      left++;
     }
-    quickSort(arr,start,right-1);
-    quickSort(arr,left,end);
+    while (right > start && arr[right] >= arr[pivot]) {
+      right--;
+    }
+    if (left > right) {
+      swap(arr, pivot, right);
+    } else {
+      swap(arr, left, right);
+    }
+  }
+  quickSort(arr, start, right - 1);
+  quickSort(arr, left, end);
 }
 
-let arr = [];
-for (let i = 0; i < 10; i++)
-  arr.push(Math.floor(Math.random() * 100));
+const arr = [
+  13, 34, 92, 22, 80,
+  67, 92, 52, 21, 86
+]
+// for (let i = 0; i < 10; i++) {
+//   arr.push(Math.floor(Math.random() * 100));
+// }
 
 console.log(arr);
-quickSort(arr,0,arr.length-1);
+quickSort(arr, 0, arr.length - 1);
 console.log(arr);
