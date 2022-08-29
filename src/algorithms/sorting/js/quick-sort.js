7c7afc22b56c6ec8a937a0327ba77752a3aafc4e
전성현 <sunghyun1148@gmail.com>
2022-08-29 21:09:08 +0900

    주석 추가

--- src/algorithms/sorting/js/quick-sort.js
+++ src/algorithms/sorting/js/quick-sort.js
@@ -10,20 +10,26 @@ function quickSort(arr, start, end) {
   let left = start + 1;
   let right = end;
 
+  // 모든 요소를 비교하교 right, left가 어긋날 때 까지
   while (right >= left) {
 
+    // left가 end보다 작거나 같고, left 위치의 요소가 피벗보다 작다면 left + 1
     while (left <= end && arr[left] <= arr[pivot]) {
       left++;
     }
+    // right가 start 보다 크고, right 위치의 요소가 피벗보다 크다면 right - 1
     while (right > start && arr[right] >= arr[pivot]) {
       right--;
     }
+    // 모든 요소를 비교했다면 right과 피벗 교환
     if (left > right) {
       swap(arr, pivot, right);
+    // 아니면 left, right 요소 교환
     } else {
       swap(arr, left, right);
     }
   }
+  // 재귀
   quickSort(arr, start, right - 1);
   quickSort(arr, left, end);
 }
