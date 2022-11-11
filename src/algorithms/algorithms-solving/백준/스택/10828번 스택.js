95bd1b627ffe1ef6948c54f97d52d94c44de5753
전성현 <sunghyun1148@gmail.com>
2022-11-12 00:08:05 +0900

    풀이 수정

--- src/algorithms/algorithms-solving/백준/스택/10828번 스택.js
+++ src/algorithms/algorithms-solving/백준/스택/10828번 스택.js
@@ -1,23 +1,30 @@
-const arr = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').slice(1);
+// 백준 10828번 스택 https://www.acmicpc.net/problem/10828
+// console.log()는 시간이 많이 걸리므로 사용 최소화 할 것
+const arr = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n").slice(1);
 const stack = [];
-
+const answer = [];
 for (let i = 0; i < arr.length; i++) {
-  if (arr[i].split(" ").length > 1) {
-    stack.push(Number(arr[i].split(" ")[1]));
-    
-  } else if (arr[i] === "top") {
-        if(stack.length > 0) console.log(stack[stack.length - 1]);
-        else console.log(-1);
-    
-  } else if (arr[i] === "empty") {
-        if(stack.length === 0) console.log(1);
-        else console.log(0);
-
-  } else if (arr[i] === "size") {
-        console.log(stack.length);
-    
-  } else if (arr[i] === "pop") {
-        if(stack.length > 0) console.log(stack.pop());
-        else console.log(-1);
+  const command = arr[i].split(" ");
+  switch (command[0]) {
+    case "push":
+      stack.push(command[1]);
+      break;
+    case "top":
+      if (stack.length > 0) answer.push(stack[stack.length - 1]);
+      else answer.push(-1);
+      break;
+    case "empty":
+      if (stack.length === 0) answer.push(1);
+      else answer.push(0);
+      break;
+    case "size":
+      answer.push(stack.length);
+      break;
+    case "pop":
+      if (stack.length > 0) answer.push(stack.pop());
+      else answer.push(-1);
+      break;
   }
 }
+
+console.log(answer.join("\n"));
