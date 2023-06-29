bf4da54838212cb68a582cc18f04bbf21b676efd
전성현 <sunghyun1148@gmail.com>
2023-06-29 23:53:55 +0900

    1996번 풀이 수정

--- src/algorithms/algorithms-solving/백준/구현(Implementation)/1966번 프린터 큐.js
+++ src/algorithms/algorithms-solving/백준/구현(Implementation)/1966번 프린터 큐.js
@@ -1,35 +1,34 @@
 // 백준 구현 실버3 https://www.acmicpc.net/problem/1966
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').slice(1).map(n => n.split(' ').map(Number));
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(n => n.split(' ').map(Number));
 
-function solution(docs) {
-  const answer = [];
+function printerQueue(inputs, target) {
 
-  for (let i = 0; i < docs.length; i += 2) {
-    // 문서 리스트 항목이 하나라면 1번 push
-    if (docs[i + 1].length === 1) {
-      answer.push(1);
-      continue;
-    }
-    // 내림차순 정렬시킨 문서 리스트 복사
-    const sorted = [...docs[i + 1]].sort((a, b) => b - a);
-    let docsLength = docs[i][0];
-    let [times, docsIndex] = [0, 0];
-    while (1) {
-      if (docs[0] === sorted[0]) {
-        docs.shift();
-        sorted.shift();
-        answer.push(times);
-        docsIndex = - 1;
-      } else {
-        const notTarget = docs.shift();
-        docs.push(notTarget);
-        times += 1;
+  const printList = inputs.map((priority, index) => ({
+    priority,
+    isTarget: index === target
+  }));
+
+  let count = 0;
+
+  while (true) {
+    const current = printList.shift();
+    const hasHighPriority = printList.some(item => item.priority > current.priority);
+
+    if (hasHighPriority) {
+      printList.push(current);
+    } else {
+      count += 1;
+
+      if (current.isTarget) {
+        return count;
       }
-
-      docsIndex = (docsIndex - 1) % docsLength;
     }
   }
-
-  return answer;
 }
-console.log(solution(input));
+
+const answer = [];
+
+for (let i = 1; i <= input.length - 1; i += 2) {
+  answer.push(printerQueue(input[i + 1], input[i][1]));
+}
+console.log(answer.join('\n'));
