275f1a00da376b0c167e4776cf08b43c6f426355
전성현 <sunghyun1148@gmail.com>
2023-08-03 22:34:42 +0900

    백준 12852번 풀이 추가

--- src/algorithms/algorithms-solving/백준/그래프/12852번 1로 만들기2.js
+++ src/algorithms/algorithms-solving/백준/그래프/12852번 1로 만들기2.js
@@ -1,6 +1,7 @@
 // 백준 그래프 탐색 실버1 https://www.acmicpc.net/problem/12852
 const input = require('fs').readFileSync('/dev/stdin').toString().trim();
 
+// 풀이 1 - 시간 다소 오래걸림
 function solution(input) {
   const answer = [];
   let min = +Infinity;
@@ -38,5 +39,40 @@ function solution(input) {
   }
 }
 
-
 console.log(solution(+input));
+
+// dp를 이용한 풀이 - 메모리 약 2배 소요
+function makeToOne2(N) {
+  const dp = new Array(N + 1).fill(0); // dp[i]: 숫자 i를 1로 만들기 위한 최소 연산 횟수
+  const operation = new Array(N + 1).fill(null); // operation[i]: 숫자 i로 가기 위해 이전 숫자 저장
+
+  for (let i = 2; i <= N; i++) {
+    dp[i] = dp[i - 1] + 1;
+    operation[i] = i - 1;
+
+    if (i % 2 === 0 && dp[i] > dp[i / 2] + 1) {
+      dp[i] = dp[i / 2] + 1;
+      operation[i] = i / 2;
+    }
+
+    if (i % 3 === 0 && dp[i] > dp[i / 3] + 1) {
+      dp[i] = dp[i / 3] + 1;
+      operation[i] = i / 3;
+    }
+  }
+
+  let current = N;
+  const path = [current];
+
+  while (current !== 1) {
+    current = operation[current];
+    path.push(current);
+  }
+
+  return [dp[N], path.join(" ")];
+}
+
+const [minOperations, steps] = makeToOne2(+input);
+
+console.log(minOperations);
+console.log(steps);
