93ad0ce429891a3fc545ffcd9621ff129dfd3d30
전성현 <sunghyun1148@gmail.com>
2024-01-13 23:51:21 +0900

    코드 수정

--- src/algorithms/tree/DFS/합이 같은 부분집합.js
+++ src/algorithms/tree/DFS/합이 같은 부분집합.js
@@ -4,18 +4,16 @@
  * @returns {number[][]} 합이 같은 부분집합들
  */
 function solution(nums) {
-  // 재귀 방문여부 체크 배열
-  const check = new Array(nums.length - 1).fill(false);
-
-  // 비교할 값 = 전체 집합 합 / 2
-  let sum = nums.reduce((acc, n) => acc + n, 0) / 2;
-  let answer = [];
-
+  const check = new Array(nums.length).fill(false);   // 재귀 방문여부 체크 배열
+  const sum = nums.reduce((acc, n) => acc + n, 0) / 2;   // 전체 집합 합 / 2
+  const answer = [];
+  
   function DFS(lv, sub) {
-    // 트리 높이를 넘어섰거나 or 부분집합 합이 나머지보다 큰 경우 종료
-    if (lv > nums.length || sub > sum) {
+    if (sub > sum) return;
+    // DFS로 모두 탐색한 경우
+    if (lv === nums.length ) {
 
-      // but 합이 같은 부분집합이라면 answer에 삽입
+      // 합이 같은 서로소 집합 중 하나라면 answer에 삽입
       if (sub === sum) {
         const tmp = [];
         for (let i = 0; i < check.length; i++){
